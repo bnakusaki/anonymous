830ed8392d3eebc6926fb56eac1a205f1a8f20d3
bnakusaki <akusakibenjaminnartey@gmail.com>
2023-05-13 07:03:10 +0100

    first commit

---
+++ core/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from django.contrib.auth.models import User
+from django.views.decorators.csrf import csrf_exempt
+# Create your views here.
+
+
+@csrf_exempt
+def demoPage(request):
+    user: User = User.objects.get(pk=1)
+    username = request.POST.get('username')
+    email = request.POST.get('email')
+    password = request.POST.get('password')
+    return JsonResponse({'name': username, 'email': email})
